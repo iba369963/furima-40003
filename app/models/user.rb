f2083644a917ba7538ecfebd568873671581388b
iba369963 <148754726+iba369963@users.noreply.github.com>
2023-11-24 20:31:17 +0900

    正規表現の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,9 +12,9 @@ class User < ApplicationRecord
     presence: true,
     length: { minimum: 6 },
     format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/}
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龠々ー]+\z/ }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龠々ー]+\z/ }
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶ]+\z/ }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶ]+\z/ }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/ }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/ }
        
 end

3307abaf923ed062f74a532406dd8c5374f26bcd
iba369963 <148754726+iba369963@users.noreply.github.com>
2023-11-24 12:05:16 +0900

    バリデーションの設定と、エラーメッセージの表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :password,
+    presence: true,
+    length: { minimum: 6 },
+    format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/, message: 'は半角英数字混合で入力してください' }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龠々ー]+\z/ }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龠々ー]+\z/ }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶ]+\z/ }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶ]+\z/ }
+       
 end

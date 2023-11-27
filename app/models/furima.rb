2735c3bc0cc672df63259cafa36ab45c9876a223
iba369963 <148754726+iba369963@users.noreply.github.com>
2023-11-27 17:25:06 +0900

    カテゴリー、状態、負担、地域のプルダウンの表示

--- app/models/furima.rb
+++ app/models/furima.rb
@@ -1,4 +1,10 @@
 class Furima < ApplicationRecord
   belongs_to :user
   has_one_attachsd :image
+  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :sales_status
+  belongs_to :shipping_fee_status
+  belongs_to :prefecture
 end

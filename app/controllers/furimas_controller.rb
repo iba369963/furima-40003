f2cd87fda3b5a8ce2cc22c03a8dbef5360d85c68
iba369963 <148754726+iba369963@users.noreply.github.com>
2023-11-29 20:39:45 +0900

    商品の削除機能を実装した。

--- app/controllers/furimas_controller.rb
+++ app/controllers/furimas_controller.rb
@@ -1,7 +1,7 @@
 class FurimasController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
   before_action :check_owner, only: [:edit, :update]
-  before_action :set_furima, only: [:show, :edit, :update]
+  before_action :set_furima, only: [:show, :edit, :update, :destroy]
 
   def index
     @furimas = Furima.includes(:user).order("created_at DESC")
@@ -35,6 +35,11 @@ class FurimasController < ApplicationController
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @furima.destroy
+    redirect_to root_path
   end
 
   private

fdb7d3a0c82342db7cb72e4b475e68b5a28f53af
iba369963 <148754726+iba369963@users.noreply.github.com>
2023-12-01 20:19:57 +0900

    foremオブジェクトファイルへの記述

---
+++ app/models/order.rb
@@ -0,0 +1,15 @@
+class Order
+  include ActiveModel::Model
+  attr_accessor :user_id, :furima_id, :postal_code, :prefecture_id, :city, :addresses, :home_name, :phone_number, :log_id
+
+  with_options presence: true do
+    validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/, message: "は「3桁ハイフン4桁」の形式で入力してください" }
+    validates :prefecture_id, numericality: { other_than: 1 }
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "は10桁以上11桁以内の半角数値で入力してください" }
+  end
+
+  def save
+    log = Log.create(user_id: user_id, furima_id: furima_id)
+    Destination.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, addresses: addresses, home_name: home_name, phone_number: phone_number, log_id: log.id)
+  end
+end
